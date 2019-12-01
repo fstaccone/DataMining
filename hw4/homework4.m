fd6e5512b2a41795a374f7c960cadc15a138ba09
fstaccone <francesco.staccone@mail.polimi.it>
2019-12-01 15:17:51 +0100

    updated hw4

--- hw4/homework4.m
+++ hw4/homework4.m
@@ -2,63 +2,54 @@ clc;
 clear all;
 close all;
 
+%Importing comma separated edge
 edges = csvread('example1.dat');
-%edges = csvread('example2.dat');
-K = 0;          %Number of clusters
-sigma = 0;     %Scaling parameter, for creating the affinity matrix. Paper says there is a way to choose it
-                %automatically. This controls how rapidly the affinity
-                %matrix falls off with the distance between the vertices
+%edges = csvread('example2.dat');        
 
-%Following the algorithm,
-%STEP 1: generating the affinity matrix, as per the equation
-%for i=1:length(edges(:,1))
-%     for j=1:length(edges(:,1))
-%        if i==j %Zero if the indexes are the same
-%            A(i,j)=0;
-%        else
-%            A(i,j)=exp(-(edges(i,1)-edges(j,1))^2/(2*sigma^2));
-%        end
-%    end
-%end
-%CRAP - this is already a graph, so we can easily construct A like so from
-%ADJACENCY MATRIX
+%STEP 1: generating the affinity matrix
+
+%Converting Edge list to adjacency matrix
 column1 = edges(:,1);
 column2 = edges(:,2);
 
 graphed = graph(column1, column2);
-A = adjacency(graphed); %This is actually easier than the above formula
-G = graph(A, 'OmitSelfLoops');           %We want to plot this later
+%Returns the sparse adjacency matrix for graph 'graphed'.
+A = adjacency(graphed); 
+%We want to plot this later
+G = graph(A, 'OmitSelfLoops');           
 
+%plots the sparsity pattern of matrix A. Nonzero values are colored while zero values are white. The plot displays the number of nonzeros in the matrix, nz = nnz(A).
 spy(A);
 title('Affinity/Sparsity matrix');
-%From here with we can see there are about 4 communities, which is why we
-%set the number of clusters K = 4
+%From here with we can see there are about 4 communities, therefore we set the number of clusters K = 4
 K=4;
 
 %STEP 2: defining D and constructing L (the Fiedler vector)
-D = diag(sum(A,2));     %Defining D as per the formula
-L = D^(-0.5)*A*D^(-0.5);
 
+%Defining D: sum(A,2) is column vector containing the sum of each row of A.
+D = diag(sum(A,2));    
+%%Defining the normalized Laplacian:
+L = D^(-0.5)*A*D^(-0.5);
 %The Laplacian is simply
 Laplacian = D - A;
 
 %STEP 3: getting the k largest eigenvectors and create matrix X
-[X, eigenVals] = eigs(L, K, 'LM');  %Returns the K largest real magnitude
-                                        %eigenvalues in the diagonal of the
-                                        %second term (eigenVals), and eigenvector columns in
-                                        %the first one (X)
 
-%STEP 4: Form the matrix Y (normalizing each row of X)
+%returns the diagonal matrix eigenVals containing the K Largest Magnitude ('LM') eigenvalues of L on the main diagonal,
+%and matrix X whose columns are the corresponding eigenvectors.
+[X, eigenVals] = eigs(L, K, 'LM');  
+
+%STEP 4: Form the matrix Y from X by normalizing each of X's rows to have unit length  
+
 Y = X./sqrt(sum(X.^2,2));
 
-%STEP 5 & 6: Taking each row in Y as a point and clustering them into K cluster
-%via kmeans
-[idx, C] = kmeans(Y, K, 'MaxIter', 100);    %Returns a vector of cluster indices
-                                            %for each observation (idx) and
-                                            %k cluster centroids (C)
+%STEP 5 & 6: Taking each row in Y as a point and clustering them into K clusters
+
+%kmeans returns a vector of cluster indices (idx) for each observation  and K cluster centroid locations in the K-by-p matrix C
+[idx, C] = kmeans(Y, K, 'MaxIter', 1000);   
                                            
-%Further PLOTTING of other stuff
-%Now, plotting the sparsity matrix again but with the clusters colored
+%Further PLOTTING 
+%Plotting the sparsity matrix again but with the clusters colored
 daColor = hsv(K);   %colormap
 figure;
 hold on;
@@ -73,7 +64,7 @@ end
 hold off;
 title('Affinity/Sparsity matrix (with the clusters highlighted)');
 
-%Plotting the graph
+%Plotting the graph G
 figure;
 P1 = plot(G, 'layout', 'force', 'Marker', '.', 'MarkerSize', 10);
 title('Graph');
@@ -89,8 +80,12 @@ for i = 1: K
 end
 
 %Plotting the Fiedler vector
-[eigenVecs,eigenVals] = eigs(Laplacian, 2, 'sa');   %Return the two smallest eigenvalues
-Fiedler = eigenVecs(:, 2);                  %Fiedler is the vector corresponding to the 2nd smallest eigenvalue
+
+%returns the diagonal matrix eigenVals containing the 2 smallest real ('sa') eigenvalues of Laplacian on the main diagonal,
+%and matrix eigenVecs whose columns are the corresponding eigenvectors.
+[eigenVecs,eigenUals] = eigs(Laplacian, 2, 'sa'); 
+%Fiedler is the vector corresponding to the 2nd smallest eigenvalue
+Fiedler = eigenVecs(:, 2);                  
 sortedFiedler = sort(Fiedler);
 figure;
 plot(sortedFiedler,'.');
